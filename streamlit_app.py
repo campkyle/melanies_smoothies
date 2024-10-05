eb8f8924f57a5540312266f925d5ca342893a1de
campkyle <deokha.kim@gmail.com>
2024-10-05 11:11:11 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,7 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
-# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
